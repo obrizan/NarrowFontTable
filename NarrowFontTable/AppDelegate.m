9bf9cd44a6c4d35a02f97812a20091086ba56eb8
Vladimir Obrizan <obrizan@dnt-lab.com>
2013-03-30 12:22:36 +0200

    Стандартный шрифт.

--- NarrowFontTable/AppDelegate.m
+++ NarrowFontTable/AppDelegate.m
@@ -8,6 +8,8 @@
 
 #import "AppDelegate.h"
 
+#import "TableViewController.h"
+
 @implementation AppDelegate
 
 - (void)dealloc
@@ -21,6 +23,9 @@
     self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
+	
+	self.window.rootViewController = [[TableViewController alloc] initWithStyle:UITableViewStylePlain];
+	
     [self.window makeKeyAndVisible];
     return YES;
 }
---
+++ NarrowFontTable/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  NarrowFontTable
+//
+//  Created by Vladimir Obrizan on 30.03.13.
+//  Copyright (c) 2013 Design and Test Lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@end
