d95be1263d298babafbb997a80e4044e31d01534
Vladimir Obrizan <obrizan@dnt-lab.com>
2013-03-30 11:52:57 +0200

    Initial Commit

---
+++ NarrowFontTable/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NarrowFontTable
+//
+//  Created by Vladimir Obrizan on 30.03.13.
+//  Copyright (c) 2013 Design and Test Lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
